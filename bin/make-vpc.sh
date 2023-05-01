2d6d5e5fa4855dd26b41f80a057ca0a14680fcd9
rdavis542 <rdavis542@verizon.net>
2023-05-01 11:03:20 -0400

    updates to make-vpc.sh

--- bin/make-vpc.sh
+++ bin/make-vpc.sh
@@ -6,3 +6,22 @@ ls -lahtr .
 terraform init 
 
 echo "Init complete"
+
+echo ""
+echo "******************************************************************"
+
+terraform fmt
+
+echo "******************************************************************"
+echo ""
+echo "Format complete"
+echo ""
+echo "******************************************************************"
+
+terraform plan --var-file=/home/runner/work/aws-vpc/aws-vpc/terraform/terraform-vpc.tfvars
+
+echo "******************************************************************"
+echo ""
+echo "Plan complete"
+echo ""
+echo "******************************************************************"
