aa9af71c0219fce6eb270b9c119a0daea0d3c83f
rdavis542 <rdavis542@verizon.net>
2023-05-02 23:45:11 -0400

    remote s3 backend attempt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "tfstategit"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+
 resource "aws_vpc" "main" {
 
   cidr_block           = var.cidr_block
