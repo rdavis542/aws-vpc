18cfb78f5072a29ec02de43142e445f711e908cb
rdavis542 <rdavis542@verizon.net>
2023-12-06 23:32:34 -0500

    update provider info

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,23 @@
 terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    aws = {
+      version = ">= 5.0.0"
+      source = "hashicorp/aws"
+    }
+  }
   backend "s3" {
     bucket         = "tfstategit"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
+}
+
+
+provider "aws" {
+
+  region = var.region
+
 }
 
 
