3c2f4be38eccceef127c51a52e79a951abc2321f
rdavis542 <rdavis542@verizon.net>
2023-12-11 20:01:26 -0500

    remove NAT GW

--- terraform/main.tf
+++ terraform/main.tf
@@ -152,15 +152,15 @@ resource "aws_route_table_association" "public_route_table_association_b" {
   
 #}
 
-resource "aws_vpc_endpoint" "s3_private_endpoint" {
+#resource "aws_vpc_endpoint" "s3_private_endpoint" {
 
-  vpc_id = aws_vpc.main.id
-  service_name = "com.amazonaws.us-east-1.s3" 
+#  vpc_id = aws_vpc.main.id
+#  service_name = "com.amazonaws.us-east-1.s3" 
   
-}
+#}
 
-resource "aws_vpc_endpoint_route_table_association" "s3_private_endpoint_vpc_associaton_a" {
-  route_table_id = aws_route_table.private_route_table_a.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3_private_endpoint.id
+#resource "aws_vpc_endpoint_route_table_association" "s3_private_endpoint_vpc_associaton_a" {
+#  route_table_id = aws_route_table.private_route_table_a.id
+#  vpc_endpoint_id = aws_vpc_endpoint.s3_private_endpoint.id
   
-}
+#}
