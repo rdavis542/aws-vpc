3a9ca6a6e4c64a08491a81d8691a154ce3d727fb
rdavis542 <rdavis542@verizon.net>
2024-01-26 22:11:02 -0500

    update route tables

--- terraform/public_routes.tf
+++ terraform/public_routes.tf
@@ -34,24 +34,18 @@ resource "aws_route_table_association" "public_route_table_association_b" {
 }
 
 
-resource "aws_route_table" "public_route_table_a" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway.id
-  }
+#resource "aws_route_table" "public_route_table_a" {
+#  vpc_id = aws_vpc.main.id
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_nat_gateway.nat_gateway.id
+#  }
 
-  tags = {
-    Name = "main-east-ig-private-route-table"
-  }
+#  tags = {
+#    Name = "main-east-ig-public-route-table"
+#  }
 
-}
-
-resource "aws_route_table_association" "public_route_table_association_a" {
-  subnet_id      = aws_subnet.vpc-public-subnet_a.id
-  route_table_id = aws_route_table.public_route_table_a.id
-
-}
+#}
 
 #resource "aws_nat_gateway" "nat_gateway" {
 #  allocation_id = aws_eip.nat_gateway_eip.id
@@ -59,16 +53,3 @@ resource "aws_route_table_association" "public_route_table_association_a" {
 #  depends_on = [ aws_internet_gateway.main-east-ig ]
 
 #}
-
-resource "aws_vpc_endpoint" "s3_private_endpoint" {
-
-  vpc_id       = aws_vpc.main.id
-  service_name = "com.amazonaws.us-east-1.s3"
-
-}
-
-resource "aws_vpc_endpoint_route_table_association" "s3_private_endpoint_vpc_associaton_a" {
-  route_table_id  = aws_route_table.private_route_table_a.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3_private_endpoint.id
-
-}
