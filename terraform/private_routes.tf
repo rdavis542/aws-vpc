376289f86441b6cc1276626f3021591544100968
rdavis542 <rdavis542@verizon.net>
2024-02-01 00:06:44 -0500

    updates to files

--- terraform/private_routes.tf
+++ terraform/private_routes.tf
@@ -23,12 +23,11 @@ resource "aws_eip" "nat_gateway_eip" {
 
 }
 
-#resource "aws_nat_gateway" "nat_gateway" {
-#  allocation_id = aws_eip.nat_gateway_eip.id
-#  subnet_id = aws_subnet.vpc-public-subnet_a.id
-#  depends_on = [ aws_internet_gateway.main-east-ig ]
-
-#}
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id = aws_subnet.vpc-public-subnet_a.id
+  depends_on = [ aws_internet_gateway.main-east-ig ]
+}
 
 resource "aws_vpc_endpoint" "s3_private_endpoint" {
 
