62927731025b50df729cb867a1faa927686bb6ea
rdavis542 <ryan_davis542@outlook.com>
2025-07-28 22:45:32 -0400

    vpc changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,22 +11,32 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "public_subnet_id" {
-  description = "ID of the public subnet"
-  value       = aws_subnet.public.id
+output "public_subnet_id_A" {
+  description = "ID of the public subnet A"
+  value       = aws_subnet.vpc-public-subnet_a.id
 }
 
-output "private_subnet_id" {
-  description = "ID of the private subnet"
-  value       = aws_subnet.private.id
+output "public_subnet_id_B" {
+  description = "ID of the public subnet B"
+  value       = aws_subnet.vpc-public-subnet_b.id
+}
+
+output "private_subnet_id_A" {
+  description = "ID of the private subnet A"
+  value       = aws_subnet.vpc-public-subnet_a.id
+}
+
+output "private_subnet_id_A" {
+  description = "ID of the private subnet B"
+  value       = aws_subnet.vpc-public-subnet_b.id
 }
 
 output "internet_gateway_id" {
   description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.main.id
+  value       = aws_internet_gateway.main-east-ig.id
 }
 
 output "nat_gateway_id" {
   description = "ID of the NAT Gateway"
-  value       = aws_nat_gateway.main.id
+  value       = aws_nat_gateway.nat_gateway.id
 }
