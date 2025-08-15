5b7f49776d455d33030d16331843e863a6eac5e0
rdavis542 <ryan_davis542@outlook.com>
2025-08-14 20:54:37 -0400

    remove NAT GW

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -36,7 +36,7 @@ output "internet_gateway_id" {
   value       = aws_internet_gateway.main-east-ig.id
 }
 
-output "nat_gateway_id" {
-  description = "ID of the NAT Gateway"
-  value       = aws_nat_gateway.nat_gateway.id
-}
+# output "nat_gateway_id" {
+#   description = "ID of the NAT Gateway"
+#   value       = aws_nat_gateway.nat_gateway.id
+# }
